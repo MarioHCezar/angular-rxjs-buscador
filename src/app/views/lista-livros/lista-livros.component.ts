f2343a7b6a80bae6addc7205fa84dbed0b8bb5ea
MarioHCezar <mariohcezar@gmail.com>
2024-04-30 00:32:19 -0300

    "to-do renderizar resultados

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { tap } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro, VolumeInfo } from 'src/app/models/interface';
 import { BuscaLivroService } from 'src/app/services/busca-livro.service';
 
 @Component({
@@ -7,16 +8,44 @@ import { BuscaLivroService } from 'src/app/services/busca-livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
 
   campoBusca: string = '';
 
+  subscription: Subscription;
+
+  livro: Livro;
+
   constructor(private service: BuscaLivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (retornoAPI) => console.log(retornoAPI),
-    );
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => this.listaLivros,
+      error: (error) => console.error(error),
+      complete: () => console.log('Search complete'),
+    });
+  }
+
+  resultadoPesquisaLivros(items) {
+    const livros: Livro[] = [];
+    items.forEach((item) => {
+      livros.push(
+        (this.livro = {
+          title: item.volumeInfo?.title,
+          authors: item.volumeInfo?.authors,
+          publisher: item.volumeInfo?.publisher,
+          publishedDate: item.volumeInfo?.publishedDate,
+          description: item.volumeInfo?.description,
+          pageCount: item.volumeInfo?.pageCount,
+          previewLink: item.volumeInfo?.previewLink,
+          thumbnail: item.volumeInfo?.imageLinks.thumbnail,
+        })
+      );
+      return livros;
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
